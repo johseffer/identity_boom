ffa0322e88161af4d8fa333f5bdcb6af964e8d8e
Johseffer <johseffer@gmail.com>
2023-04-28 15:38:36 -0300

    fix installs

--- boom.py
+++ boom.py
@@ -4,29 +4,39 @@ import time
 import os
 import subprocess
 
+subprocess.check_output('sudo apt update',shell=True)
+
 try:
     check_net_tools = subprocess.check_output('dpkg -s net-tools', shell=True)
     if str('install ok installed') in str(check_net_tools):
         pass
 except subprocess.CalledProcessError:
-    print('[+] net-tools not installed')
-    subprocess.check_output('sudo apt update',shell=True)
+    print('[+] net-tools not installed. installing...')    
     subprocess.check_output('sudo apt install net-tools -y', shell=True)
     print('[!] net-tools installed succesfully')
+
+try:
+    check_iptables = subprocess.check_output('dpkg -s iptables', shell=True)
+    if str('install ok installed') in str(check_iptables ):
+        pass
+except subprocess.CalledProcessError:
+    print('[+] iptables not installed. installing...')
+    subprocess.check_output('sudo apt install iptables -y', shell=True)
+    print('[!] iptables installed succesfully')
 
 try:
     check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
     if str('install ok installed') in str(check_pip3):
         pass
 except subprocess.CalledProcessError:
-    print('[+] pip3 not installed')
+    print('[+] pip3 not installed. installing...')
     subprocess.check_output('sudo apt install python3-pip -y', shell=True)
     print('[!] pip3 installed succesfully')
 
 try:
     import requests
 except Exception:
-    print('[+] python3 requests is not installed')
+    print('[+] python3 requests is not installed. installing...')
     os.system('pip3 install requests')
     os.system('pip3 install requests[socks]')
     print('[!] python3 requests is installed ')
@@ -35,7 +45,7 @@ try:
     check_tor = subprocess.check_output('which tor', shell=True)
 except subprocess.CalledProcessError:
 
-    print('[+] tor is not installed !')
+    print('[+] tor is not installed. installing...')
     subprocess.check_output('sudo apt update',shell=True)
     subprocess.check_output('sudo apt install tor -y',shell=True)
     print('[!] tor is installed succesfully ')
@@ -44,7 +54,7 @@ try:
     check_anonym8 = subprocess.check_output('which anonym8', shell=True)
     
     if check_anonym8 == 'anonym8 not found':
-        print('[+] anonym8 is not installed !')        
+        print('[+] anonym8 is not installed. installing...')        
         subprocess.check_output('sudo bash install_anonym8.sh',shell=True)
         print('[!] anonym8 is installed succesfully ')    
     
